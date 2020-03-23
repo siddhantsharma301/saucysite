2ce1d15bdd99c6cfd30de17c2bda9126f449135f
Siddhant Sharma <sharmasiddhant301@gmail.com>
2020-03-23 00:16:45 -0700

    add more social

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import { Link, graphql } from "gatsby"
 
 import "../styles/index.css"
 import { ReactComponent as GitHub } from "../svg/github.svg";
+import { ReactComponent as LinkedIn } from "../svg/linkedin.svg";
+import { ReactComponent as Twitter } from "../svg/twitter.svg";
 
 export const query = graphql`
 query  {
@@ -12,17 +14,29 @@ query  {
           github
           linkedin
           twitter
+          instagram
         }
       }
     }
   }
 `
 
-export default () => <>
+export default ({ data }) => <>
     <h1>Siddhant Sharma</h1>
     <p>A high school senior from the East Bay.</p>
     <p>Loves making cool, useful things.</p>
-    <a className="github-icon">
+    <a className="github-icon" href={'https://github.com/' + data.site.siteMetadata.social.github}>
         <GitHub/> 
     </a>
-</>
+    <a className="linkedin-icon" href={'https://linkedin.com/in/' + data.site.siteMetadata.social.linkedin}>
+        <LinkedIn/>
+    </a>
+    <a className="twitter-icon" href={'https://twitter.com/' + data.site.siteMetadata.social.twitter}>
+        <Twitter/>
+    </a>    
+</> 
+
+
+// <a className="instagram-icon" href={'https://instagram.com/' + data.site.siteMetadata.social.instagram}>
+// <Instagram/> 
+// </a>
