c63f35ec3aba340f8d2b18c0b99a0d075b9d08a8
Siddhant Sharma <sharmasiddhant301@gmail.com>
2020-03-23 16:54:01 -0700

    added an about me section

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import { ReactComponent as GitHub } from "../svg/github.svg";
 import { ReactComponent as LinkedIn } from "../svg/linkedin.svg";
 import { ReactComponent as Twitter } from "../svg/twitter.svg";
 import { ReactComponent as Instagram } from "../svg/instagram.svg"
+import { ReactComponent as Water }from "../svg/water.svg"
 
 export const query = graphql`
 query  {
@@ -24,8 +25,8 @@ query  {
 
 export default ({ data }) => <>
     <h1>Siddhant Sharma</h1>
-    <p>A high school senior from the East Bay.</p>
-    <p>Loves making cool, useful things.</p>
+    <p className="intro">A high school senior from the East Bay.</p>
+    <p className="intro">Loves making cool, useful things.</p>
 
     <div className="social">
     <a className="linkedin-icon" href={'https://linkedin.com/in/' + data.site.siteMetadata.social.linkedin}>
@@ -41,6 +42,16 @@ export default ({ data }) => <>
           <Instagram/> 
       </a>
     </div>
+
+  <div className="row">
+    <div className="column">
+      <a className="water-icon">
+        <Water/>
+      </a>
+    </div>
+    <div className="column">
+      <h2>About Me</h2>
+      <p className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse faucibus aliquet mauris, in vehicula dui rhoncus sed. Donec at pharetra enim. Donec ac tincidunt purus. Nam fermentum rhoncus vestibulum. </p>
+    </div>
+  </div>
 </> 
-
-
